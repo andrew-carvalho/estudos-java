230b129cf70b6fdf74a4db9ac66ba783305baa9d
Andrew Ignácio <andrewignacio@outlook.com.br>
2025-01-26 13:58:30 -0300

    feat: add Aula 01 - Variáveis

---
+++ src/topico01fundamentos/aula01variaveis/Program.java
@@ -0,0 +1,36 @@
+package topico01fundamentos.aula01variaveis;
+
+public class Program {
+    public static void main(String[] args) {
+        /*
+         * Tipos inteiros:
+         * byte: 8 bits
+         * short: 16 bits
+         * int: 32 bits
+         * long: 64 bits
+         * */
+        int age = 21;
+
+        /*
+         * Tipos de ponto flutuante:
+         * float: 32 bits
+         * double: 64 bits
+         * */
+        double height = 1.74;
+
+        /*
+         * Tipo caractere:
+         * char: 16 bits
+         * */
+        char sex = 'M';
+
+        /*
+         * Tipo booleano:
+         * boolean: 1 bit
+         * */
+        boolean isProgrammer = true;
+
+        // String não é um tipo permitido e permite o armazenamento de textos
+        String name = "Andrew";
+    }
+}
